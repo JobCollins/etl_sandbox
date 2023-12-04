0aa5cdfa14c61ec75a5ea66cf4e5921f87bcc13b
Job D. Collins <dulo.collins@gmail.com>
2023-12-04 12:59:33 +0300

    Automate (#4)
    * Bug: KafkaTimeoutError: Failed to update metadata after 60.0 secs.
    
    producer works intermittently
    investigate memory allocation issue
    
    * BugFix: KafkaTimeoutError: Failed to update metadata after 60.0 secs
    
    * Feature: Pipeline that sends the data to AWS S3
    
    sending data from producer to aws S3 bucket
    
    * Feature: Kafka MLOPs Weather Streaming App: Producer
    
    producer for sending message to create weather_forecasts topic
    using random module to randomly pick weather and weather elements
    
    * Feature: MLOPs with Kafka: Consumer
    
    kafka consumer to consume the data produced by the kafka producer
    consumes the random data produced every 5 seconds and visualizes the data

---
+++ mlopsKafka/weather_streaming/kafka_consumer.py
@@ -0,0 +1,33 @@
+from kafka import KafkaConsumer
+import matplotlib.pyplot as plt
+
+# create the consumer 
+consumer = KafkaConsumer('weather_forecasts', bootstrap_servers = 'localhost:9092')
+
+temperatures, humidities = [], []
+
+plt.ion() # turn on interactive mode for plot
+
+while True:
+    for message in consumer:
+        data = message.value.decode()
+        temp = int(data.split("Temperature: ")[1].split("C")[0])
+        humidity = int(data.split("Humidity: ")[1].split("%")[0])
+
+        temperatures.append(temp)
+        humidities.append(humidity)
+
+        if len(temperatures) > 50:
+            temperatures.pop(0)
+            humidities.pop(0)
+
+        plt.clf() # clear the plot
+        plt.subplot(2,1,1)
+        plt.plot(temperatures, label="Temperature (deg C)")
+        plt.legend()
+
+        plt.subplot(2,1,2)
+        plt.plot(humidities, label="Humidity (%)")
+        plt.legend()
+
+        plt.pause(5) # refresh every 5 seconds
